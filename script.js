4b37ba16b7d64c01f3d577d71aa13a51cc485f3c
= <=>
2021-11-28 15:01:41 +0530

    making squareData and one square from that data

--- script.js
+++ script.js
@@ -4,6 +4,27 @@ let svg = d3
   .attr("height", window.innerHeight)
   .attr("width", window.innerWidth);
 
+let squareData = {
+  height: 35,
+  width: 35,
+  x: 0,
+  y: 0,
+  color: "blue",
+  margin: 0,
+};
+
+svg
+  .selectAll("rect")
+  .data([squareData])
+  .enter()
+  .append("rect")
+  .attr("height", (d) => d.height)
+  .attr("width", (d) => d.width)
+  .attr("fill", (d) => d.color)
+  .attr("x", (d) => d.x)
+  .attr("y", (d) => d.y)
+  .attr("margin", (d) => d.margin);
+
 setInterval(() => {
   let time = new Date();
   let h = time.getHours();
