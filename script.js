21c3c7a1c8f6f57d9eb8481b9e2c9dddcde0afed
= <=>
2021-11-28 14:51:51 +0530

    13 making svg and set interval

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hello");
+let svg = d3
+  .select(".container")
+  .append("svg")
+  .attr("height", window.innerHeight)
+  .attr("width", window.innerWidth);
+
+setInterval(() => {
+  let time = new Date();
+  let h = time.getHours();
+  let m = time.getMinutes();
+  let s = time.getSeconds();
+}, 1000);
